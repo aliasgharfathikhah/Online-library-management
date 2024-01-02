2265a8d9cd524ce6c36eafc901cdcb1e7b0f50e5
ali fathi <ali.fathi.13121110@gmail.com>
2024-01-02 13:18:34 +0330

    fix Add_Order to Add_Order_Or_User_Add_Order

--- online-library-management/order/urls.py
+++ online-library-management/order/urls.py
@@ -2,6 +2,6 @@ from django.urls import path, include
 from . import views
 
 urlpatterns = [
-    path('add', views.Add_Order),
+    path('add', views.Add_Order_Or_User_Add_Order),
     path('show', views.Show_All_Orders),
 ]
--- online-library-management/order/views.py
+++ online-library-management/order/views.py
@@ -4,19 +4,34 @@ import datetime
 from order.models import order
 from books.models import Header
 
-def Add_Order(request):
-    if request.method == 'POST':
-        form = AddOrderForm(request.POST)
-        if form.is_valid():
-            order = form.save(commit=False)
-            order.time = form.cleaned_data['time']
-            order.save()
-            return redirect('/')
+def Add_Order_Or_User_Add_Order(request):
+    if request.user.is_staff:
+        if request.method == 'POST':
+            form = AddOrderForm(request.POST)
+            if form.is_valid():
+                order = form.save(commit=False)
+                order.time = form.cleaned_data['time']
+                order.save()
+                return redirect('/')
+        else:
+            form = AddOrderForm()
+
+        return render(request, 'html/add_order.html', {
+            'form': form
+        })
     else:
-        form = AddOrderForm()
+        if request.method == 'POST':
+            form2 = AddOrderForm(request.POST)
+            if form2.is_valid():
+                order = form2.save(commit=False)
+                order.time = form2.cleaned_data['time']
+                order.save()
+                return redirect('/')
+        else:
+            form2 = AddOrderForm()
 
     return render(request, 'html/add_order.html', {
-        'form': form
+        'form2': form2
     })
 
 def Show_All_Orders(request):
