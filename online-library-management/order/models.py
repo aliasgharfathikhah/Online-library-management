db630917989a41dd659939408ebcff87d7b1158f
ali fathi <ali.fathi.13121110@gmail.com>
2024-01-02 13:04:50 +0330

    add model order_from_request_user in order

--- online-library-management/order/models.py
+++ online-library-management/order/models.py
@@ -6,4 +6,10 @@ class order(models.Model):
     user = models.ForeignKey(User, on_delete=models.SET_NULL, null=True)
     book = models.ForeignKey(Book, on_delete=models.SET_NULL, null=True)
     time = models.DateField()
+
+class order_from_request_user(models.Model):
+    user = models.ForeignKey(User, on_delete=models.SET_NULL, null=True)
+    book = models.ForeignKey(Book, on_delete=models.SET_NULL, null=True)
+    time = models.DateField()
+    status = models.BooleanField(default=False)
     
